c24529417f07fe81ec4f584bf802fd18983bc055
Ryan Belgrave <ryan@belgrave.me>
2024-08-09 09:04:52 -0500

    module path

--- auth/cert/tailscale/roles.tf
+++ auth/cert/tailscale/roles.tf
@@ -1,6 +1,6 @@
 resource "vault_cert_auth_backend_role" "cert" {
   name                 = "foo"
-  certificate          = file("./le_isrg_root_x2.pem")
+  certificate          = file("${path.module}/le_isrg_root_x2.pem")
   backend              = vault_auth_backend.cert.path
   allowed_common_names = ["foo.example.org"]
   display_name         = ""
