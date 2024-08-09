661b10f1e35dc3b865672d4fab7f7088b33e8351
Ryan Belgrave <ryan@belgrave.me>
2024-08-09 09:06:42 -0500

    more tailscale cert stuff

--- auth/cert/tailscale/roles.tf
+++ auth/cert/tailscale/roles.tf
@@ -2,8 +2,8 @@ resource "vault_cert_auth_backend_role" "cert" {
   name                 = "foo"
   certificate          = file("${path.module}/le_isrg_root_x2.pem")
   backend              = vault_auth_backend.cert.path
-  allowed_common_names = ["foo.example.org"]
-  display_name         = ""
+  allowed_common_names = ["woo.tailnet-047c.ts.net"]
+  display_name         = "tailscale_machine_woo"
   token_policies       = ["default"]
-  token_bound_cidrs    = [""]
+  token_bound_cidrs    = ["1.1.1.1"]
 }
