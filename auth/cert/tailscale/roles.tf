85b239725f62b4fef600ecc0835b6cefbf21a780
Ryan Belgrave <ryan@belgrave.me>
2024-08-09 10:44:22 -0500

    try and output tailscale servers

--- auth/cert/tailscale/roles.tf
+++ auth/cert/tailscale/roles.tf
@@ -1,17 +1,21 @@
 data "tailscale_devices" "devices" {}
 
 locals {
-  tailscale_servers = [for each in data.tailscale_devices.devices.devices : each if contains(each.tags, "servers")]
+  tailscale_servers = toset([for each in data.tailscale_devices.devices.devices : each if contains(each.tags, "servers")])
 }
 
-resource "vault_cert_auth_backend_role" "tailscale_server_role" {
-  for_each = local.tailscale_servers
-
-  name                 = each.key.name
-  certificate          = file("${path.module}/le_isrg_root_x2.pem")
-  backend              = vault_auth_backend.cert.path
-  allowed_common_names = [each.key.name]
-  display_name         = each.key.name
-  token_policies       = ["default"]
-  token_bound_cidrs    = each.key.addresses
+output "tailscale_servers" {
+  value = tailscale_servers
 }
+
+# resource "vault_cert_auth_backend_role" "tailscale_server_role" {
+#   for_each = local.tailscale_servers
+
+#   name                 = each.key.name
+#   certificate          = file("${path.module}/le_isrg_root_x2.pem")
+#   backend              = vault_auth_backend.cert.path
+#   allowed_common_names = [each.key.name]
+#   display_name         = each.key.name
+#   token_policies       = ["default"]
+#   token_bound_cidrs    = each.key.addresses
+# }
