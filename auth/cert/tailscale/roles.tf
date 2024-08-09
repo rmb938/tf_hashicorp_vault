db73dd90cc6c0563a8607651a3ef021d04f1a6c7
Ryan Belgrave <ryan@belgrave.me>
2024-08-09 10:58:27 -0500

    ok what about now

--- auth/cert/tailscale/roles.tf
+++ auth/cert/tailscale/roles.tf
@@ -10,14 +10,14 @@ resource "null_resource" "tailscale_servers" {
   }
 }
 
-# resource "vault_cert_auth_backend_role" "tailscale_server_role" {
-#   for_each = local.tailscale_servers
+resource "vault_cert_auth_backend_role" "tailscale_server_role" {
+  for_each = local.tailscale_servers
 
-#   name                 = each.key.name
-#   certificate          = file("${path.module}/le_isrg_root_x2.pem")
-#   backend              = vault_auth_backend.cert.path
-#   allowed_common_names = [each.key.name]
-#   display_name         = each.key.name
-#   token_policies       = ["default"]
-#   token_bound_cidrs    = each.key.addresses
-# }
+  name                 = each.key.name
+  certificate          = file("${path.module}/le_isrg_root_x2.pem")
+  backend              = vault_auth_backend.cert.path
+  allowed_common_names = [each.key.name]
+  display_name         = each.key.name
+  token_policies       = ["default"]
+  token_bound_cidrs    = each.key.addresses
+}
