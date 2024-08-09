5007071c9efc37a3d88bb52d65b0233366713d03
Ryan Belgrave <ryan@belgrave.me>
2024-08-09 11:20:16 -0500

    confusion

--- auth/cert/tailscale/roles.tf
+++ auth/cert/tailscale/roles.tf
@@ -2,9 +2,7 @@ data "tailscale_devices" "devices" {}
 
 locals {
   tailscale_servers = {
-    for each in data.tailscale_devices.devices.devices : each => {
-      each.name : each
-    }
+    for each in data.tailscale_devices.devices.devices : each.name => each if contains(each.tags, "tag:servers")
   }
 }
 
@@ -15,7 +13,7 @@ resource "null_resource" "tailscale_servers" {
 }
 
 resource "vault_cert_auth_backend_role" "tailscale_server_role" {
-  for_each = { for name, device in local.tailscale_servers : name => device if contains(device.tags, "tag:servers") }
+  for_each = local.tailscale_servers
 
   name                 = each.value.name
   certificate          = file("${path.module}/le_isrg_root_x2.pem")
