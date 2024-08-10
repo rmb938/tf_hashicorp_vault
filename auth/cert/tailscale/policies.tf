7958ec49f5e155091c1cf2807e317d7d11f77244
Ryan Belgrave <ryan@belgrave.me>
2024-08-10 15:48:54 -0500

    add vault policy for consul server

--- auth/cert/tailscale/policies.tf
+++ auth/cert/tailscale/policies.tf
@@ -5,5 +5,61 @@ resource "vault_policy" "ts_default" {
 path "secret/data/consul/encrypt_key" {
   capabilities = ["read"]
 }
+
+path "/sys/mounts/pki_consul_root" {
+  capabilities = [ "read" ]
+}
+path "/sys/mounts/pki_consul_intermediate_0" {
+  capabilities = [ "read" ]
+}
+path "/sys/mounts/pki_consul_intermediate_0/tune" {
+  capabilities = [ "update" ]
+}
+path "/pki_consul_root/" {
+  capabilities = [ "read" ]
+}
+path "/pki_consul_root/root/sign-intermediate" {
+  capabilities = [ "update" ]
+}
+path "/pki_consul_intermediate_0/*" {
+  capabilities = [ "create", "read", "update", "delete", "list" ]
+}
+path "auth/token/renew-self" {
+  capabilities = [ "update" ]
+}
+path "auth/token/lookup-self" {
+  capabilities = [ "read" ]
+}
+EOT
+}
+
+resource "vault_policy" "ts_hashiconsulserver" {
+  name = "ts_hashiconsulserver"
+
+  policy = <<EOT
+path "/sys/mounts/pki_consul_root" {
+  capabilities = [ "read" ]
+}
+path "/sys/mounts/pki_consul_intermediate_0" {
+  capabilities = [ "read" ]
+}
+path "/sys/mounts/pki_consul_intermediate_0/tune" {
+  capabilities = [ "update" ]
+}
+path "/pki_consul_root/" {
+  capabilities = [ "read" ]
+}
+path "/pki_consul_root/root/sign-intermediate" {
+  capabilities = [ "update" ]
+}
+path "/pki_consul_intermediate_0/*" {
+  capabilities = [ "create", "read", "update", "delete", "list" ]
+}
+path "auth/token/renew-self" {
+  capabilities = [ "update" ]
+}
+path "auth/token/lookup-self" {
+  capabilities = [ "read" ]
+}
 EOT
 }
