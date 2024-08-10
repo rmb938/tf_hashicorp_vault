4a6ab0140b967bb23349e28113ae6c5203531f38
Ryan Belgrave <ryan@belgrave.me>
2024-08-10 14:43:25 -0500

    create consul root pki (#6)

---
+++ secret_engine/pki_consul.tf
@@ -0,0 +1,23 @@
+resource "vault_mount" "pki_consul_root" {
+  path = "pki_consul_root"
+  type = "pki"
+}
+
+resource "vault_pki_secret_backend_root_cert" "pki_consul_root" {
+  backend     = vault_mount.pki_consul_root.path
+  type        = "internal"
+  common_name = "Consul Root"
+  ttl         = "631139040" # 20 years
+}
+
+resource "vault_pki_secret_backend_issuer" "pki_consul_root" {
+  backend     = vault_pki_secret_backend_root_cert.pki_consul_root.backend
+  issuer_ref  = vault_pki_secret_backend_root_cert.pki_consul_root.issuer_id
+  issuer_name = "Consul Root"
+}
+
+resource "vault_pki_secret_backend_config_issuers" "pki_consul_root" {
+  backend                       = vault_mount.pki_consul_root.path
+  default                       = vault_pki_secret_backend_issuer.pki_consul_root.issuer_id
+  default_follows_latest_issuer = true
+}
