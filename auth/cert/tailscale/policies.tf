838fa2895efe9b5a78a318e618b106f8385dabdc
Ryan Belgrave <ryan@belgrave.me>
2024-08-10 19:40:12 -0500

    need to update acl for encrypt key

--- auth/cert/tailscale/policies.tf
+++ auth/cert/tailscale/policies.tf
@@ -2,7 +2,7 @@ resource "vault_policy" "ts_default" {
   name = "ts_default"
 
   policy = <<EOT
-path "secret/data/consul/encrypt_key" {
+path "secret/consul/encrypt_key" {
   capabilities = ["read"]
 }
 EOT
