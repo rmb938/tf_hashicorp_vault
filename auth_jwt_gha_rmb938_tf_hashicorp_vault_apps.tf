63348b4c1ed2a84c0b10e7fae111a0b3f4414e14
Ryan Belgrave <ryan@belgrave.me>
2025-02-09 16:07:51 -0600

    need consul mgmt key

--- auth_jwt_gha_rmb938_tf_hashicorp_vault_apps.tf
+++ auth_jwt_gha_rmb938_tf_hashicorp_vault_apps.tf
@@ -2,21 +2,25 @@ resource "vault_policy" "gha_rmb938_tf_hashicorp_vault_apps" {
   name = "gha_rmb938_tf_hashicorp_vault_apps"
 
   policy = <<EOT
-  path "auth/token/create" {
-    capabilities = ["update"]
-  }
+path "secret/consul/management_token" {
+  capabilities = ["read"]
+}
 
-  path "sys/policies/*" {
-    capabilities = ["create", "read", "update", "delete"]
-  }
+path "auth/token/create" {
+  capabilities = ["update"]
+}
 
-  path "auth/step-cert/certs/*" {
-    capabilities = ["create", "read", "update", "delete"]
-  }
+path "sys/policies/*" {
+  capabilities = ["create", "read", "update", "delete"]
+}
 
-  path "consul/roles/*" {
-    capabilities = ["create", "read", "update", "delete"]
-  }
+path "auth/step-cert/certs/*" {
+  capabilities = ["create", "read", "update", "delete"]
+}
+
+path "consul/roles/*" {
+  capabilities = ["create", "read", "update", "delete"]
+}
 EOT
 }
 
