390b9d535df5d9b20baa0b73fac2cc54d613e5e4
Ryan Belgrave <ryan@belgrave.me>
2025-02-12 09:34:29 -0600

    confused

--- secret_engine_pki_step_x5c_haproxy.tf
+++ secret_engine_pki_step_x5c_haproxy.tf
@@ -102,7 +102,7 @@ resource "vault_pki_secret_backend_config_issuers" "config" {
   backend = vault_mount.pki_step_x5c_haproxy_root.path
 
   # Always set the default issuer to the latest one created
-  default                       = vault_pki_secret_backend_intermediate_set_signed.pki_step_x5c_haproxy_intermediate[pki_step_x5c_haproxy_intermediates - 1].imported_issuers[0]
+  default                       = vault_pki_secret_backend_intermediate_set_signed.pki_step_x5c_haproxy_intermediate[0].imported_issuers[0]
   default_follows_latest_issuer = false
 }
 
