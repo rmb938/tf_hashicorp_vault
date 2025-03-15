c51a8a69f2b96083da933f8a76b3126f6a339961
Ryan Belgrave <ryan@belgrave.me>
2025-03-15 14:53:21 -0500

    no min decryption atm

--- secret_engine_transit_openstack_keystone_token.tf
+++ secret_engine_transit_openstack_keystone_token.tf
@@ -17,7 +17,7 @@ resource "vault_transit_secret_backend_key" "transit_openstack_keystone_token" {
   exportable             = false
   allow_plaintext_backup = false
 
-  min_decryption_version = 0
+  # min_decryption_version = 0
   min_encryption_version = 0
 
   auto_rotate_period = 604800 // 1 week
