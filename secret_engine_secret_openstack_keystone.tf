b90a1b6738aa4a50e69eefc66ec0c132dcd6b465
Ryan Belgrave <ryan@belgrave.me>
2025-04-14 17:01:20 -0500

    need to include secret mount path

--- secret_engine_secret_openstack_keystone.tf
+++ secret_engine_secret_openstack_keystone.tf
@@ -1,5 +1,5 @@
 resource "vault_kv_secret" "openstack_keystone_service_user_cinder" {
-  path = "openstack-keystone/expected-service-users/cinder"
+  path = "${vault_mount.secret.path}/openstack-keystone/expected-service-users/cinder"
   data_json = jsonencode({
     foo = "bar"
   })
