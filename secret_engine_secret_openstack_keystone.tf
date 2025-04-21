b26acf71c5ac0d41287e3eaeba3ae5867af18d69
Ryan Belgrave <ryan@belgrave.me>
2025-04-21 18:41:38 -0500

    add nova controller and compute, add neutron compute

--- secret_engine_secret_openstack_keystone.tf
+++ secret_engine_secret_openstack_keystone.tf
@@ -26,8 +26,35 @@ resource "vault_kv_secret" "openstack_keystone_service_user_neutron" {
   depends_on = [vault_mount.secret]
 }
 
+resource "vault_kv_secret" "openstack_keystone_service_user_neutron_compute" {
+  path = "${vault_mount.secret.path}/openstack-keystone/expected-service-users/neutron-compute"
+  data_json = jsonencode({
+    foo = "bar"
+  })
+
+  depends_on = [vault_mount.secret]
+}
+
 resource "vault_kv_secret" "openstack_keystone_service_user_placement" {
   path = "${vault_mount.secret.path}/openstack-keystone/expected-service-users/placement"
+  data_json = jsonencode({
+    foo = "bar"
+  })
+
+  depends_on = [vault_mount.secret]
+}
+
+resource "vault_kv_secret" "openstack_keystone_service_user_nova_controller" {
+  path = "${vault_mount.secret.path}/openstack-keystone/expected-service-users/nova-controller"
+  data_json = jsonencode({
+    foo = "bar"
+  })
+
+  depends_on = [vault_mount.secret]
+}
+
+resource "vault_kv_secret" "openstack_keystone_service_user_nova_compute" {
+  path = "${vault_mount.secret.path}/openstack-keystone/expected-service-users/nova-compute"
   data_json = jsonencode({
     foo = "bar"
   })
