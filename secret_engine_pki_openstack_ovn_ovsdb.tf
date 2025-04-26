7d21224e5cd1c536b658155cb69f68633d675010
Ryan Belgrave <ryan@belgrave.me>
2025-04-26 14:31:01 -0500

    Update secret_engine_pki_openstack_ovn_ovsdb.tf

--- secret_engine_pki_openstack_ovn_ovsdb.tf
+++ secret_engine_pki_openstack_ovn_ovsdb.tf
@@ -191,3 +191,23 @@ resource "vault_pki_secret_backend_role" "pki_openstack_ovn_ovsdb_intermediate_u
   no_store            = true
   not_before_duration = "30s"
 }
+
+resource "vault_pki_secret_backend_role" "pki_openstack_ovn_ovsdb_intermediate_user_neutron_compute" {
+  backend             = vault_mount.pki_openstack_ovn_ovsdb_intermediate.path
+  name                = "user-neutron-controller"
+  issuer_ref          = "default"
+  ttl                 = "7776000" # 90 days
+  max_ttl             = "7776000"
+  allow_ip_sans       = false
+  allowed_domains     = ["neutron-compute"]
+  allow_bare_domains  = true
+  allow_subdomains    = false
+  enforce_hostnames   = false
+  server_flag         = false
+  client_flag         = true
+  key_type            = "ec"
+  key_bits            = 256
+  generate_lease      = false
+  no_store            = true
+  not_before_duration = "30s"
+}
